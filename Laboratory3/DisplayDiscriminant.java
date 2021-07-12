b307537e2033d2fa679fa5e142ae35ece84bdb32
PhiThai1309 <s3878070@rmit.edu.vn>
2021-07-12 21:23:25 +0700

    QuadraticEquation

---
+++ Laboratory3/DisplayDiscriminant.java
@@ -0,0 +1,48 @@
+/*
+Design a class named QuadraticEquation for a quadratic equation ax2 + bx + c = 0. The
+class contains:
+• Private attributes a, b, c that represent three coefficients.
+• A constructor with the arguments for a, b, and c.
+• Three get methods for a, b, c.
+• A method getDiscriminant() that return the discriminant b^2 – 4ac.
+• Methods getRoot1() and getRoot2() for returning two roots of the equation
+These methods are useful only if the discriminant is non negative. Let these
+methods return 0 if the discriminant is negative.
+Draw a class diagram for this class and then implement the class. Write a test program
+that prompts the user to enter values for a, b then displays the result based on the
+discriminant. If the discriminant is positive, display two roots. If the discriminant is 0,
+display one root. Otherwise, display "The equation has no roots."
+ */
+package Laboratory3;
+
+import java.util.Scanner;
+
+public class DisplayDiscriminant {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the value for A: ");
+        double a = input.nextDouble();
+
+        System.out.println("Enter the value for B: ");
+        double b = input.nextDouble();
+
+        System.out.println("Enter the value for C: ");
+        double c = input.nextDouble();
+
+        QuadraticEquation equation = new QuadraticEquation(a,b,c);
+        double discriminant = equation.getDiscriminant();
+
+        if(discriminant > 0) {
+            System.out.println("The roots are " + equation.getRoot1() + " and " + equation.getRoot2());
+        } else if (discriminant == 0) {
+            System.out.println("The root is " + equation.getRoot1());
+        } else if (discriminant < 0) {
+            System.out.println("The equation has no roots");
+        }
+    }
+}
+
+
+
---
+++ Laboratory3/QuadraticEquation.java
@@ -0,0 +1,54 @@
+package Laboratory3;
+
+public class QuadraticEquation {
+    private double a;
+    private double b;
+    private double c;
+
+    //Constructor
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    //Getters and setters
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    //Get Discriminant
+    public double getDiscriminant() {
+        return Math.pow(b, 2) - 4 * a * c;
+    }
+
+    public double getRoot1() {
+        return getDiscriminant() < 0 ? 0 :
+                ((-b) + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    }
+
+    public double getRoot2() {
+        return getDiscriminant() < 0 ? 0 :
+                ((-b) - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    }
+}
